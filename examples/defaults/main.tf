3365334c16d8da757b4ca2c3772740e18be94efb
rob <robcannonxyz@gmail.com>
2020-11-12 15:14:43 -0800

    adding example

--- examples/defaults/main.tf
+++ examples/defaults/main.tf
@@ -6,6 +6,11 @@ provider "aws" {
   region = var.aws_region
 }
 
+variable "private_key_path" {}
+variable "public_key_path" {}
+
 module "defaults" {
-  source = "../.."
+  source           = "../.."
+  private_key_path = var.private_key_path
+  public_key_path  = var.public_key_path
 }
